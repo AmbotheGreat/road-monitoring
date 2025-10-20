5c7e6b812cce8ae93f5ae5cbc2f2de1449ba4754
Joshua Lina <lina.joshuacruz@gmail.com>
2025-10-20 20:13:02 +0800

    Update Navbar component to use new Tailwind CSS syntax for background opacity in mobile overlay.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
         {/* Mobile Overlay */}
         {isSidebarOpen && (
           <div
-            className="fixed inset-0 bg-black bg-opacity-50 z-30 lg:hidden"
+            className="fixed inset-0 bg-black/50 z-30 lg:hidden"
             onClick={() => setIsSidebarOpen(false)}
           />
         )}
